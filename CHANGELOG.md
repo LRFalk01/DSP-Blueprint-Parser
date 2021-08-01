96d68497efa947f840378311feba571901ea7e84
Lucas Falk <Lucas.Falk@datalinksoftware.com>
2021-07-31 22:17:53 -0400

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2021-07-31
+
+- Initial release
---
+++ lib/dsp_blueprint_parser.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dsp_blueprint_parser/version"
+
+module DspBlueprintParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
